b340baba84a8f2942b9570996dd415e9870c253f
twino <twino2@naver.com>
2022-02-04 17:23:42 +0900

    220204javaStudy

---
+++ src/com/urdomain/lecture/ch02/LongExample.java
@@ -0,0 +1,16 @@
+package com.urdomain.lecture.ch02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		//vlong var3 = 100000000000;		//컴파일 에러
+		long var4 = 100000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
