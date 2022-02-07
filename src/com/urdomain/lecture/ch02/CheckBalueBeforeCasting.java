5f86c372bf9b270773d02ad0e0634ac46cdf2ee9
twino <twino2@naver.com>
2022-02-07 17:30:45 +0900

    220207 java

---
+++ src/com/urdomain/lecture/ch02/CheckBalueBeforeCasting.java
@@ -0,0 +1,36 @@
+package com.urdomain.lecture.ch02;
+
+public class CheckBalueBeforeCasting {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 127;
+		
+		System.out.println("Byte.MIN_VALUE: " + Byte.MIN_VALUE); // 상수
+		System.out.println("Byte.MAX_VALUE: " + Byte.MAX_VALUE);
+		System.out.println("Short.MIN_VALUE: " + Short.MIN_VALUE); // 상수
+		System.out.println("Short.MAX_VALUE: " + Short.MAX_VALUE);
+		System.out.println("Integer.MIN_VALUE: " + Integer.MIN_VALUE); // 상수
+		System.out.println("Integer.MAX_VALUE: " + Integer.MAX_VALUE);
+		System.out.println("Long.MIN_VALUE: " + Long.MIN_VALUE); // 상수
+		System.out.println("Long.MAX_VALUE: " + Long.MAX_VALUE);
+		System.out.println("Float.MIN_VALUE: " + Float.MIN_VALUE); // 상수
+		System.out.println("Float.MAX_VALUE: " + Float.MAX_VALUE);
+		System.out.println("Double.MIN_VALUE: " + Double.MIN_VALUE); // 상수
+		System.out.println("Double.MAX_VALUE: " + Double.MAX_VALUE);
+		
+		
+		
+		if( (i<Byte.MIN_VALUE) || (i>Byte.MAX_VALUE) ) {
+			System.out.println("Byte 타입으로 변환할 수 없습니다.");
+			System.out.println("값을 다시 확인해 주세요");
+		} else {
+			Byte b = (byte) i;
+			System.out.println(b);
+			System.out.println("정상적으로 캐스팅 되었습니다.");
+			
+		}
+	}
+
+}
---
+++ src/com/urdomain/lecture/ch02/PromotionExample.java
@@ -0,0 +1,44 @@
+package com.urdomain.lecture.ch02;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		
+		
+		// 작은 놈이 큰놈으로 변환은 문제가 발생하지않는다.
+		// 하지만 큰놈이 작은놈으로 변환이 되려면 문제가 생긴다.
+		
+		
+		// byte < short < int < long
+		// float < double
+		
+		byte byteValue = 10;
+		System.out.println("intValue: " + byteValue);
+		
+		int intValue = byteValue;
+		System.out.println("intValue: " + intValue);
+
+		char charValue = '가';
+		System.out.println("charValue: " + charValue);
+		
+		intValue = charValue;
+		System.out.println("가의 유니코드=" + intValue);
+		
+		intValue = 500;
+		long longValue = intValue;
+		System.out.println("longValue: " + longValue);
+		
+		intValue = 200;
+		float floatValue = intValue;
+		System.out.println("floatValue: " + floatValue);
+		
+		intValue = 200;
+		double doubleValue = intValue;
+		System.out.println("doubleValue: " + doubleValue);
+		
+		
+		
+		
+	}
+
+}
