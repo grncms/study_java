cf49d01301b97d7229091285419490679c50a21e
twino <twino2@naver.com>
2022-02-10 17:42:02 +0900

    220210java

--- src/com/urdomain/lecture/ch4/IfNestedExample.java
+++ src/com/urdomain/lecture/ch4/IfNestedExample.java
@@ -7,29 +7,49 @@ public class IfNestedExample {
 		int score = 60;
 		String grade = "";
 		
+//		if(score >= 90) {
+//			if(score >= 95) {
+//				grade = "A+";
+//			} else {
+//				grade = "A";
+//			}
 		if(score >= 90) {
 			if(score >= 95) {
 				grade = "A+";
-			} else {
+			} if(score >= 90) {
 				grade = "A";
+			} if(score >= 85) {
+				grade = "B+";
+			} if(score >= 80) {
+				grade = "B";
+			} if(score >= 75) {
+				grade = "C+";
+			} if(score >= 70) {
+				grade = "C";
+			} if(score >= 65) {
+				grade = "D+";
+			} if(score >= 60) {
+				grade = "D";
+			} else {
+				grade = "F";
 			}
-
-			
-		} else if (score < 90) {
-			grade = "B+"; 
-		} else if (score < 85) {
-			grade = "B"; 
-		} else if (score < 80) {
-			grade = "C+"; 
-		} else if (score < 75) {
-			grade = "C"; 
-		} else if (score < 70) {
-			grade = "D+"; 
-		} else if (score < 65) {
-			grade = "D"; 
-		} else {
-			grade = "F"; 
 		}
+			
+//		} else if (score < 90) {
+//			grade = "B+"; 
+//		} else if (score < 85) {
+//			grade = "B"; 
+//		} else if (score < 80) {
+//			grade = "C+"; 
+//		} else if (score < 75) {
+//			grade = "C"; 
+//		} else if (score < 70) {
+//			grade = "D+"; 
+//		} else if (score < 65) {
+//			grade = "D"; 
+//		} else {
+//			grade = "F"; 
+//		}
 		System.out.println("학점 : " + grade);
 	}
 
