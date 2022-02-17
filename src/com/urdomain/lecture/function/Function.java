79b34f35e089af4be5df8b16435512291d638a87
twino <twino2@naver.com>
2022-02-17 16:00:16 +0900

    java

---
+++ src/com/urdomain/lecture/function/Function.java
@@ -0,0 +1,60 @@
+package com.urdomain.lecture.function;
+
+public class Function {
+
+	public static void main(String[] args) {
+
+		arithmeticOperation(3, 4);
+
+	}
+
+	public static int add(int a, int b) {
+
+		int result = a + b;
+		return result;
+	}
+
+	public static void arithmeticOperation(int a, int b) {
+
+		int plus = a + b;
+		int minus = a - b;
+		int multiply = a * b;
+		int divide = a / b;
+
+		System.out.println("a + b = " + plus);
+		System.out.println("a - b = " + minus);
+		System.out.println("a * b = " + multiply);
+		System.out.println("a / b = " + divide);
+
+	}
+	
+//		오늘 뭐 먹지 프로그램
+//		배열 사용
+//		함수 사용
+//		출력을 위한 함수도 별도로 제작
+//		
+//		
+//		한식, 양식, 일식, 중식 
+//		오늘 추천 드리는 메뉴는 ?? 입니다.
+	
+	
+	
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/urdomain/lecture/function/QueenOfcoding.java
@@ -0,0 +1,10 @@
+package com.urdomain.lecture.function;
+
+public class QueenOfcoding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
