70065d546ada1ed040e3d11cabadc93ce287820a
twino <twino2@naver.com>
2022-02-17 17:40:35 +0900

    jaajav

---
+++ src/com/urdomain/lecture/ch06/Car.java
@@ -0,0 +1,19 @@
+package com.urdomain.lecture.ch06;
+
+public class Car {
+
+//	field
+	String company = "현대자동차";
+	String model = "벨로스터";
+	String color = "gray";
+	
+	int maxSpeed = 240;
+	int speed = 0;
+	
+	
+//	method
+	
+	
+	
+	
+}
---
+++ src/com/urdomain/lecture/ch06/UsedTrade.java
@@ -0,0 +1,20 @@
+package com.urdomain.lecture.ch06;
+
+import java.util.Date;
+
+public class UsedTrade {
+// field
+	
+	String item = "";
+	Integer price = 0;
+	Integer negotiation = 0;
+	String phrchaseMethod = "";
+	String place = "";
+	Date appTime;
+	
+	
+	
+	
+	
+	
+}
