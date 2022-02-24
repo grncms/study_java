c0b6834c4b5564a4e5e269cd55b6dc4be091de6d
twino <twino2@naver.com>
2022-02-24 17:43:29 +0900

    220224java

--- src/com/urdomain/lecture/ch06/FinalCarExample.java
+++ src/com/urdomain/lecture/ch06/FinalCarExample.java
@@ -3,6 +3,11 @@ package com.urdomain.lecture.ch06;
 public class FinalCarExample {
 
 	public static void main(String[] args) {
+	
+		FinalCar finalCar = new FinarCar();
+		
+		//
+		
 		
 	}
 
---
+++ src/com/urdomain/lecture/ch07/MyFriendDetailInfoExample.java
@@ -0,0 +1,13 @@
+package com.urdomain.lecture.ch07;
+
+public class MyFriendDetailInfoExample {
+
+	public static void main(String[] args) {
+		
+		My
+		
+		
+		
+	}
+
+}
