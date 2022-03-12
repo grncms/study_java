4994e6ff32e03edf6d5f8dcac1506042240c8ea2
twino <twino2@naver.com>
2022-03-12 16:19:55 +0900

    12312313

--- src/minsujava01/Main13.java
+++ src/minsujava01/Main13.java
@@ -10,15 +10,10 @@ public class Main13 {
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
 		
 		sc.close();
 		
-		System.out.println(a*d);
-		System.out.println(a*c);
 		System.out.println(a*b);
-		System.out.println(a*d*100 + a*c*10 + a*b);
 	}
 
 }
